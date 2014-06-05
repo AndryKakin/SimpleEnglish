273f9fdf8b02de571dd250fe2d31b8e4349dda62
Ailend <andrewsw@mail.ru>
2014-06-05 20:40:27 +0400

    Test.

--- Execute/src/main/java/com/example/simpleenglish/execute/Startup.java
+++ Execute/src/main/java/com/example/simpleenglish/execute/Startup.java
@@ -4,19 +4,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Startup extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+        String str = "\u1D46" + "  TEST";
+        TextView textView = (TextView)findViewById(R.id.TestTextView);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-		
-		
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.startup, menu);
         return true;
